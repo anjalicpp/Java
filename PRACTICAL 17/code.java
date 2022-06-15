bd83137cad8078dc74e7eabb16595ca68d84e5a0
anjalicpp <91792558+anjalicpp@users.noreply.github.com>
2022-06-15 19:16:31 +0530

    Update code.java

--- PRACTICAL 17/code.java
+++ PRACTICAL 17/code.java
@@ -1,4 +1,27 @@
 Practical-17
 Write a program that reads char by char and writes in some other another file .Write the oprogram in efficient way.
+import java.io.*;
+class ReadCharByChar {
+ public static void main(String[] args) {
+ try {
+ FileInputStream file1 = new FileInputStream("Question17Input.txt");
+ BufferedInputStream f1 = new BufferedInputStream(file1);
+ FileWriter fw = new FileWriter("Question17Output.txt");
+ BufferedWriter bw = new BufferedWriter(fw);
+ int i = f1.read();
+ while (i != -1) {
+ bw.write(i);
+ i = f1.read();
+ }
+ System.out.println( "Content on Question17input.txt file is sucessfully written to
+Question17Output.txt file.");
+ f1.close();
+ bw.close();
+ }
+ catch (Exception e) {
+ System.out.println("Error: " + e);
+ }
+ }
+}
 
 ******************************************
